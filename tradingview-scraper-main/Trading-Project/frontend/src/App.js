1732f476f5c6445b747fc5cdc694477affec80d9
4995yogesh <4995yogesh@gmail.com>
2026-08-19 01:28:01 +0530

    refactor: return project dashboard ownership to research repo

--- tradingview-scraper-main/Trading-Project/frontend/src/App.js
+++ tradingview-scraper-main/Trading-Project/frontend/src/App.js
@@ -9,7 +9,6 @@ import ChartPage from './components/chart/ChartPage';
 import InfiniteCanvasPage from './components/canvas/InfiniteCanvasPage';
 import RefinementDashboard from './components/training/RefinementDashboard';
 import PatternExplorerPage from './components/patterns/PatternExplorerPage';
-import ResearchDashboard from './components/research/ResearchDashboard';
 
 const HomePage = () => {
   return (
@@ -29,15 +28,13 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<ResearchDashboard />} />
-          <Route path="/research" element={<ResearchDashboard />} />
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/chart" element={<ChartPage />} />
           <Route path="/chart/:symbol" element={<ChartPage />} />
           <Route path="/canvas" element={<InfiniteCanvasPage />} />
           <Route path="/training" element={<RefinementDashboard />} />
           <Route path="/patterns" element={<PatternExplorerPage />} />
-          <Route path="*" element={<ResearchDashboard />} />
+          <Route path="*" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </div>
